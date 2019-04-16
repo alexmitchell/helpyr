9b476dd8e8284164cc54f44550116c0a41cbf778
alex mitchell <mitchell.alexanderj@gmail.com>
2019-04-15 17:56:11 -0700

    Created a kwarg checker and figure helper modules.

---
+++ helpyr/figure_helpyr.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+
+from os.path import join as pjoin
+
+import numpy as np
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+from helpyr import kwarg_checker
+from helpyr import helpyr_misc as hpm
+
+class FigureSaver:
+    """ 
+
+    Provides basic file saving functionality that I've needed in several 
+    projects. Mostly provides convenient filename assembly options. 
+
+    """
+
+    def __init__(self, **kwargs):
+        check = kwarg_checker.get_check_kwarg_fu(kwargs)
+
+        self.figure_extension = check('figure_extension', 'png')
+        self.logger = check('logger', None)
+        self.debug_mode = check('debug_mode', False)
+
+        self.figure_root_dir = check('figure_root_dir', './')
+        sub_dir = check('figure_sub_dir', '')
+        self.figure_dir = pjoin(self.figure_root_dir, sub_dir)
+
+        hpm.ensure_dir_exists(self.figure_root_dir)
+        hpm.ensure_dir_exists(self.figure_dir)
+
+    def save_figure(self, **kwargs):
+        """
+
+        Save a figure function. Provides convenient filename assembly options. 
+
+        Kwargs:
+        'figure_name' is a complete name for a figure. It will be used directly 
+        as the filename. Can't define with 'figure_name_parts'.
+
+        'figure_name_parts' is the ordered sequence of file name chunks. The 
+        chunks will be joined with '_' to generate the file name. Can't define
+        with 'figure_name'.
+
+        'figure' is the handle for the figure to save.
+
+        'alt_subdir' is the optional alternate subdirectory to use instead of the one
+        provided in the constructor.
+
+        """
+        check = kwarg_checker.get_check_kwarg_fu(kwargs)
+
+        figure_name = check('figure_name', None)
+        figure_name_parts = check('figure_name_parts', None)
+        figure = check('figure', None)
+        alt_subdir = check('alt_subdir', None)
+
+        assert((figure_name is None) ^ (figure_name_parts is None)) # XOR
+
+        if figure_name is None:
+            figure_name = '_'.join(figure_name_parts)
+
+        # Check if an alternate subdirectory is desired
+        if alt_subdir is None:
+            destination_dir = self.figure_dir
+        else:
+            destination_dir = pjoin(self.figure_root_dir, alt_subdir)
+            hpm.ensure_dir_exists(destination_dir)
+
+        filename = f"{figure_name}.{self.figure_extension}"
+        filepath = pjoin(destination_dir, filename)
+        
+        if self.debug_mode:
+            msgs = ["!!!", f"   Not saving figure to {filepath}", "!!!"]
+            if self.logger is None:
+                for msg in msgs:
+                    print(msg)
+            else:
+                self.logger.write(msgs)
+        else:
+            msg = f"Saving figure to {filepath}"
+            if self.logger is None:
+                print(msg)
+            else:
+                self.logger.write(msg)
+
+            if figure is not None:
+                # Save target figure
+                figure.savefig(filepath, orientation='landscape')
+            else:
+                # Save current figure
+                plt.savefig(filepath, orientation='landscape')
+
+
+
+class StableSubplots:
+
+    """
+    
+    Allows for creating subplot grids and adding axes from inside a loop. 
+    The first call to add_subplot with a new figure name will create a new 
+    figure. Subsequent calls to add_subplot with the same figure name will 
+    reuse the existing figure.
+    
+    Useful for long/messy loops or if the looping structure is not in the same
+    code location as the plotting function. Keeps the plotting parameters in 
+    one location where it is relevant rather than bloating the loop 
+    initialization. 
+    
+    """
+
+    existing_plots = {} # {name : [fig_reference, axs_used]}
+    
+    def __init__(self, logger=None):
+        self.logger = logger
+        if logger is not None:
+            logger.write("Initializing a StableSubplots object")
+
+    def add_subplot(self, fig_name, **kwargs):
+        """
+
+        Add an axis to either a new or existing figure based on fig_name
+        fig_name will also be used as a filename. 
+
+        """
+
+        # Get kwargs
+        check = kwarg_checker.get_check_kwarg_fu(kwargs)
+        subplots_shape = check('subplots_shape', (1,1))
+        fig_kwargs = check('fig_kwargs', {})
+        ax_kwargs = check('ax_kwargs', {})
+        suptitle = check('suptitle', '')
+
+        nrows, ncols = subplots_shape
+
+        # Reuse existing figure or create new one
+        if fig_name in StableSubplots.existing_plots:
+            # Use existing figure
+            fig, axs = StableSubplots.existing_plots[fig_name]
+            ax_id = len(axs) + 1
+        else:
+            # Make new figure
+            fig = plt.figure(**fig_kwargs)
+            fig.suptitle(suptitle)
+            axs = np.array([])
+            ax_id = 1
+        assert(1 <= ax_id <= nrows * ncols)
+
+        # Print some output
+        is_new_str = 'new' if ax_id == 1 else 'existing'
+        msg = f"Creating new axis for {is_new_str} figure {fig_name}"
+        if self.logger is None:
+            print(msg)
+        else:
+            self.logger.write(msg)
+
+        # Create the subplot axis
+        ax = fig.add_subplot(nrows, ncols, ax_id, **ax_kwargs)
+
+        # Update existing_plots
+        axs = np.append(axs, ax)
+        StableSubplots.existing_plots[fig_name] = [fig, axs]
+
+        return ax
+
+    def share_3D_scales(self, fig_name):
+
+        fig, axs = StableSubplots.existing_plots[fig_name]
+
+        # Set up scale range for each of three dimensions
+        plot_scales = np.zeros((3, 2))
+
+        for ax in axs:
+            lim = np.array([
+                ax.get_xlim(), 
+                ax.get_ylim(),
+                ax.get_zlim(),
+                ])
+            plot_scales[:,0] = np.amin([lim[:,0], plot_scales[:,0]], axis=0)
+            plot_scales[:,1] = np.amax([lim[:,1], plot_scales[:,1]], axis=0)
+
+        for ax in axs:
+            ax.set_xlim(plot_scales[0,:]), 
+            ax.set_ylim(plot_scales[1,:]),
+            ax.set_zlim(plot_scales[2,:]),
+    def finish(self, fig_name=None, show=True, saving_fu=None):
+
+        existing = StableSubplots.existing_plots
+        target_figs = list(existing.keys()) if fig_name is None else [fig_name]
+        if saving_fu is None:
+            saver = FigureSaver(logger=self.logger)
+            saving_fu = saver.save_figure
+
+
+        # Save the target figures
+        for name in target_figs:
+            fig, axs = existing[name]
+            if '_3d_' in name:
+                self.share_3D_scales(name)
+
+            saving_fu(figure_name=name, figure=fig)
+
+        # Show figures if applicable, otherwise close them
+        if show:
+            plt.show()
+        else:
+            for name in target_figs:
+                fig, axs = existing[name]
+                plt.close(fig)
+
+        # Remove figures from the existing_plots dictionary
+        for name in target_figs.copy():
+            del StableSubplots.existing_plots[name]
+
+
--- helpyr/kwarg_checker.py
+++ helpyr/kwarg_checker.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+def check_kwarg(kwargs, name, default=None):
+    """ Simple function for checking a kwarg. Useful if you don't want to
+    create a new object to check a kwarg or two. """
+    return kwargs[name] if name in kwargs else default
+
+def get_check_kwarg_fu(kwargs):
+    """ Create a simple function for checking kwargs. Useful if you don't 
+    need long term storage of KwargChecker and find supplying kwargs to 
+    check_kwarg as an argument annoying. """
+
+    fu = lambda name, default: \
+        kwargs[name] if name in kwargs else default
+    return fu
+
 
 class KwargChecker:
     """ Simple kwarg checking class. Better ones almost certainly exist 
@@ -8,7 +22,7 @@ class KwargChecker:
     def __init__(self, kwargs):
         self.kwargs = kwargs
 
-    def kwarg_check(self, name, default):
+    def kwarg_check(self, name, default=None):
         if name in self.kwargs:
             return self.kwargs[name]
         else:
