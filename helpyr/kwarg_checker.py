0f838f52c58e5f3162659f5cf88f4183bc2193c2
Alex Mitchell <ebpvyeuj@gmail.com>
2020-08-28 16:17:50 -0700

    Added type casting and more tests.
    Tests should be completed for the current set of features. However I
    don't know how to effectively test the get_fu functions because they
    return function handles. I haven't written any tests for the
    KwargChecker class because it isn't really used any more. Delete it?

--- helpyr/kwarg_checker.py
+++ helpyr/kwarg_checker.py
@@ -14,17 +14,26 @@ def check_kwarg(kwargs, name, default=None, arg_type=None, required=False, pop=F
     if name in kwargs:
         out = kwargs.pop(name) if pop else kwargs[name]
 
-    _assert_types(out, name=name, arg_type=arg_type)
-    return out
+    type_handle = _assert_types(out, name=name, arg_type=arg_type)
+    if type_handle is None:
+        # No type casting
+        return out
+    elif isinstance(type_handle, (list, tuple)):
+        # Type casting elements in a list
+        return [th(element) for th, element in zip(type_handle, out)]
+    else:
+        # Type casting a single output variable
+        return type_handle(out)
 
 def get_check_kwarg_fu(function_kwargs, pop=False):
     """ Create a simple function for checking kwargs. Useful if you don't 
     need long term storage of KwargChecker and find supplying kwargs to 
     check_kwarg as an argument annoying. Pop will pop entries out of the 
     kwargs list if found. """
-    def fu(name, default=None, required=False):
+    def fu(name, default=None, arg_type=None, required=False):
         return check_kwarg(function_kwargs, name,
-                default=default, required=required, pop=pop)
+                default=default, arg_type=arg_type, 
+                required=required, pop=pop)
     return fu
 
 
@@ -40,9 +49,17 @@ def check_add_kwarg(kwargs, name, default=None, arg_type=None, force=False):
     if name not in kwargs or force:
         kwargs[name] = default
     
-    _assert_types(kwargs[name], name=name, arg_type=arg_type)
-
-    return kwargs[name]
+    out = kwargs[name]
+    type_handle = _assert_types(out, name=name, arg_type=arg_type)
+    if type_handle is None:
+        # No type casting
+        return out
+    elif isinstance(type_handle, (list, tuple)):
+        # Type casting elements in a list
+        return [th(element) for th, element in zip(type_handle, out)]
+    else:
+        # Type casting a single output variable
+        return type_handle(out)
 
 def get_check_add_kwarg_fu(function_kwargs):
     """ Create a simple function for checking and adding kwargs. """
@@ -53,58 +70,175 @@ def get_check_add_kwarg_fu(function_kwargs):
 
 
 
+def _generic_assert_type(arg, arg_name, type_handle, type_name, type_casting_ok):
+    """Assert if the arg is of type(s) type_handle or, if type_casting_ok is 
+    True, assert if the arg can be cast to any of type_handle.
+
+    Parameters
+    ----------
+    arg : 
+        The variable being checked.
+    arg_name : string
+        The name of the variable used for the error message.
+    type_handle : 
+        The handle for the variable type desired. Can be a single handle (e.g. 
+        type_handle=int) or a sequence of handles (e.g. type_handle=(int, 
+        float)).
+    type_name : 
+        The name of the type(s) for the error message. (e.g. 'int or float')
+    type_casting_ok:
+        Indicates whether type casting is allowed. However, the function only 
+        checks that it is possible. **The function does not actually recast the 
+        variable.** 
+
+    Returns
+    -------
+    type_handle :
+        The returned type handle will be the same as the input type_handle 
+        unless type_casting_ok is True and there are multiple accepted types. 
+        If so, the return handle will be the first successful type to cast the 
+        variable.
+    """
+    error_message = f"{arg_name} ({type(arg)}) must be of type {type_name}"
+    output_type_handle = type_handle
+    try:
+        # See if arg is of type(s) type_handle
+        assert isinstance(arg, type_handle), error_message
+
+    except AssertionError as wrong_type_error:
+        # arg is not of type type_handle
+       
+        # If type casting is not allowed, re-raise the failure
+        if not type_casting_ok:
+            raise wrong_type_error
+
+        # Type casting is allowed, see if arg can be casted to type(s) 
+        # type_handle
+        if isinstance(type_handle, (tuple, list)):
+            # Multiple accepted types, see if arg can be cast to any
+            # RETURNS the first type handle that is successful
+            for th in type_handle:
+                try:
+                    assert isinstance(th(arg), th)
+                    output_type_handle = th
+                    break
+                except:
+                    pass
+            else:
+                # Failed to find a valid casting type
+                raise wrong_type_error
+        else:
+            # Single accepted type, see if arg can be cast to it
+            try:
+                assert isinstance(type_handle(arg), type_handle)
+            except (AssertionError, ValueError, TypeError):
+                raise wrong_type_error
+
+    # Assertions all passed
+    return output_type_handle
 
 def _assert_types(arg, name='arg', arg_type=None):
-    """Check types."""
+    """Assert that arg is the correct type, if applicable.
 
-    # Assert that it is the correct type, if applicable
+    Parameters
+    ----------
+    arg :
+        The variable to be checked.
+
+    name :
+        The name of the arg to be used for error messages.
+
+    arg_type :
+        A string representing the accepted type or types for the variable. 
+        Currently handles the basic types: 'list', 'tuple', 'int', 'float', 
+        'str', and 'bool').  Can specify extra qualifiers too. Multiple 
+        types and qualifiers can be present in the string. Order only matters 
+        if checking casting between both int and float(e.g. 'int-float' vs. 
+        'float-int'), where the first ones are given preference.
+
+        Extra qualifiers:
+        'none' = Allows the arg to be None even if something else was specified
+        'tcast' = 
+            If the var does not match the provided type(s), allows the program 
+            to check if the variable can be cast to the desired type(s). 'pos', 
+            'whole', 'neg' = Asserts an 'int' or 'float' variable is >0, >=0, 
+            or <0, respectively.
+
+        Examples:
+        '' or None = No type specifications, check will unconditionally pass.
+        'float-none' = Check if the variable is a float or None.
+        'float-int-pos' = Check if variable is a positive (>0) float or int.
+        'int-whole-tcast' =
+            Check if variable is or can be cast to a whole (>=0) int.
+
+    Returns
+    -------
+    If type casting is allowed, returns the successful casting type. (First one 
+    if there are multiple accepted types) If type casting is not allowed, the 
+    return variable is not useful.
+    """
+
+    # No type requirements
     if arg_type is None or arg_type == '':
-        return # No type requirements
+        return
      
     # Check that None is allowed if the arg is still None
     elif arg is None:
         assert 'none' in arg_type, f"{name} cannot be None"
         return # arg is okay, no further checking needed
 
-    arg_type_str = f"({arg} is type {type(arg)})"
+    type_casting_ok = 'tcast' in arg_type
+    output_type_handle = None
+    is_sequence = False
 
     # Check list types
     if 'list' in arg_type or 'tuple' in arg_type:
-        assert isinstance(arg, (list, tuple))
+        is_sequence = True
+        # For either lists or tuples, assert that all elements are a valid type
 
+        assert isinstance(arg, (list, tuple))
         if isinstance(arg, list):
             assert 'list' in arg_type
-            for element in arg:
-                # Check each element
-                _assert_types(element, name=name, 
-                        arg_type=arg_type.replace('list','').strip('_- '))
-
         elif isinstance(arg, tuple):
             assert 'tuple' in arg_type
-            for element in arg:
-                # Check each element
-                _assert_types(element, name=name, 
-                        arg_type=arg_type.replace('tuple','').strip('_- '))
+
+        output_type_handle = []
+        sequence_arg_type = \
+                arg_type.replace('list','').replace('tuple', '').strip('_- ')
+        for element in arg:
+            # Check each element
+            output_type_handle.append(
+                    _assert_types(
+                        element, name=name, arg_type=sequence_arg_type)
+                    )
 
     # Check numeric types
     elif 'int' in arg_type and 'float' in arg_type:
         # int or float allowed
-        assert isinstance(arg, (int, float)), \
-                f"{name} must be a float or int {arg_type_str}"
+        # The order is set by which one comes first
+        int_idx = arg_type.index('int')
+        float_idx = arg_type.index('float')
+        handles = (int, float) if int_idx < float_idx else (float, int)
+        output_type_handle = _generic_assert_type(
+                arg, name, handles, 'float or int', type_casting_ok)
     elif 'int' in arg_type:
-        assert isinstance(arg, int), \
-                f"{name} must be a int {arg_type_str}"
+        output_type_handle = _generic_assert_type(
+                arg, name, int, 'int', type_casting_ok)
     elif 'float' in arg_type:
-        assert isinstance(arg, float), \
-                f"{name} must be a float {arg_type_str}"
+        output_type_handle = _generic_assert_type(
+                arg, name, float, 'float', type_casting_ok)
     
-    # Check string types
+    # Check other types
     elif 'str' in arg_type:
-        assert isinstance(arg, str), \
-                f"{name} must be a string {arg_type_str}"
+        output_type_handle = _generic_assert_type(
+                arg, name, str, 'string', type_casting_ok)
+    elif 'bool' in arg_type:
+        output_type_handle = _generic_assert_type(
+                arg, name, bool, 'boolean', type_casting_ok)
 
     # Unknown type requirement
     else:
+        print(f"Unknown arg type!")
         print(f"arg={arg}, name={name}, arg_type={arg_type}")
         raise NotImplementedError
 
@@ -120,6 +254,9 @@ def _assert_types(arg, name='arg', arg_type=None):
         else:
             pass # No sign requirements
 
+    # Assertions all passed. Return the output type handle.
+    # This will be useful only if type casting is allowed.
+    return output_type_handle if type_casting_ok else None
 
 
 class KwargChecker:
--- tests/test_kwarg_checker.py
+++ tests/test_kwarg_checker.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# To get html coverage report:
+# >>> pytest --cov-report html[:html_dir_name]
+#            --cov[=project_name] 
+#            [output_dir]
+
 import pytest
 from numpy import ndarray
 from helpyr.kwarg_checker import check_kwarg
@@ -43,78 +48,132 @@ def _assert_equal(a, b):
     # 'a != a and b != b' check if they are nans
     assert (a == b) or (a is b) or (a != a and b != b)
 
-@pytest.mark.parametrize("val, arg_type", [
-    ('anything', None), # No type requirements
-    (None, 'int_none'), # numeric type and None allowed
-    (None, 'float_none'), #  numeric type and None allowed
-    (None, 'int_float_none'), # numeric type and None allowed
-    (None, 'str_none'), # string type and None allowed
-    (None, 'arbitrary_none'), # arbitrary type and None allowed
-    (1, 'int'),
-    (1.0, 'float'),
-    (1, 'int_float'), # can be an int or float
-    (1.0, 'int_float'), # can be an int or float
-    (1, 'int_pos'),
-    (0, 'int_whole'),
-    (-1, 'int_neg'),
-    (1.0, 'float_pos'),
-    (0.0, 'float_whole'),
-    (-1.0, 'float_neg'),
-    ('abc', 'str'),
-    ([], 'list'),
-    ([1,2,3], 'list'),
-    ([1,2,3], 'list-int'),
-    ([1.0,2.0,3.0], 'list-float'),
-    (list('abc'), 'list'),
-    (list('abc'), 'list-str'),
-    ((1,2,3), 'tuple'),
-    ((1,2,3), 'tuple-int'),
-    ((1.0,2.0,3.0), 'tuple-float'),
-    (tuple('abc'), 'tuple'),
-    (tuple('abc'), 'tuple-str'),
-    ])
-def test__assert_types_passing(val, arg_type):
-    """Make sure _assert_types returns empty (as opposed raising an error)."""
-    assert _assert_types(val, arg_type=arg_type) is None
 
-@pytest.mark.parametrize("val, arg_type", [
-    (None, 'int'),       # Can't be None
-    (None, 'float'),     # Can't be None
-    (None, 'int_float'), # Can't be None
-    (None, 'str'),       # Can't be None
-    (None, 'arbitrary'), # Can't be None
-    (1.0, 'int'), # Must be a int
-    (1, 'float'), # Must be a float
-    ([], 'int_float'),    # Must be a int or float
-    ('abc', 'int_float'), # Must be a int or float
-    (0, 'int_pos'),        # Wrong sign
-    (-1, 'int_whole'),     # Wrong sign
-    (1, 'int_neg'),        # Wrong sign
-    (0.0, 'float_pos'),    # Wrong sign
-    (-1.0, 'float_whole'), # Wrong sign
-    (1.0, 'float_neg'),    # Wrong sign
-    (1, 'str'), # Not a string
-    (123, 'list'),    # Not a list
-    ('abc', 'list'),  # Not a list
-    ((), 'list'),     # Not a list
-    (123, 'tuple'),   # Not a tuple
-    ('abc', 'tuple'), # Not a tuple
-    ([], 'tuple'),    # Not a tuple
-    ([1,2,3], 'list-float'),     # Wrong element type
-    ([1.0,2.0,3.0], 'list-int'), # Wrong element type
-    ((1,2,3), 'tuple-float'),     # Wrong element type
-    ((1.0,2.0,3.0), 'tuple-int'), # Wrong element type
-    ])
-def test__assert_types_AssertionError(val, arg_type):
-    with pytest.raises(AssertionError):
-        _assert_types(val, arg_type=arg_type)
+class TestAssertTypes:
 
-@pytest.mark.parametrize("val, arg_type, error_type", [
-    (1234, 'new-type', NotImplementedError), # arbitrary type not allowed
-    ])
-def test__assert_types_other_errors(val, arg_type, error_type):
-    with pytest.raises(error_type):
-        _assert_types(val, arg_type=arg_type)
+    @pytest.mark.parametrize("val, arg_type", [
+    # parameters: val arg_type
+        # Check basic vars
+        ('anything', None), # No type requirements
+        (None, 'int_none'), # numeric type and None allowed
+        (None, 'float_none'), #  numeric type and None allowed
+        (None, 'int_float_none'), # numeric type and None allowed
+        (None, 'str_none'), # string type and None allowed
+        (None, 'arbitrary_none'), # arbitrary type and None allowed
+        (1, 'int'),
+        (1.0, 'float'),
+        (1, 'int_float'), # can be an int or float
+        (1.0, 'int_float'), # can be an int or float
+        (1, 'int_pos'),
+        (0, 'int_whole'),
+        (-1, 'int_neg'),
+        (1.0, 'float_pos'),
+        (0.0, 'float_whole'),
+        (-1.0, 'float_neg'),
+        ('abc', 'str'),
+        (True, 'bool'),
+
+        # Check some lists or tuples
+        ([], 'list'),
+        ([1,2,3], 'list'),
+        ([1,2,3], 'list-int'),
+        ([1.0,2.0,3.0], 'list-float'),
+        (list('abc'), 'list'),
+        (list('abc'), 'list-str'),
+        ((1,2,3), 'tuple'),
+        ((1,2,3), 'tuple-int'),
+        ((1.0,2.0,3.0), 'tuple-float'),
+        (tuple('abc'), 'tuple'),
+        (tuple('abc'), 'tuple-str'),
+        ])
+    def test__assert_types_passing(self, val, arg_type):
+        """Make sure _assert_types returns empty (as opposed raising an 
+        error)."""
+        assert _assert_types(val, arg_type=arg_type) is None
+
+
+    @pytest.mark.parametrize("val, arg_type", [
+    # parameters: val, arg_type
+        (None, 'int'),       # Can't be None
+        (None, 'float'),     # Can't be None
+        (None, 'int_float'), # Can't be None
+        (None, 'str'),       # Can't be None
+        (None, 'arbitrary'), # Can't be None
+        (1.0, 'int'), # Must be a int
+        (1, 'float'), # Must be a float
+        ([], 'int_float'),    # Must be a int or float
+        ('abc', 'int_float'), # Must be a int or float
+        (0, 'int_pos'),        # Wrong sign
+        (-1, 'int_whole'),     # Wrong sign
+        (1, 'int_neg'),        # Wrong sign
+        (0.0, 'float_pos'),    # Wrong sign
+        (-1.0, 'float_whole'), # Wrong sign
+        (1.0, 'float_neg'),    # Wrong sign
+        (1, 'str'), # Not a string
+        (None, 'bool'),  # Not a bool
+        (1, 'bool'),     # Not a bool
+        ('abc', 'bool'), # Not a bool
+
+        (123, 'list'),    # Not a list
+        ('abc', 'list'),  # Not a list
+        ((), 'list'),     # Not a list
+        (123, 'tuple'),   # Not a tuple
+        ('abc', 'tuple'), # Not a tuple
+        ([], 'tuple'),    # Not a tuple
+        ([1,2,3], 'list-float'),     # Wrong element type
+        ([1.0,2.0,3.0], 'list-int'), # Wrong element type
+        ((1,2,3), 'tuple-float'),     # Wrong element type
+        ((1.0,2.0,3.0), 'tuple-int'), # Wrong element type
+        
+        # Check some type casting
+        ('abc', 'float_tcast'),
+        ('abc', 'int_tcast'),
+        ('abc',   'int_float_tcast'),
+        ])
+    def test__assert_types_AssertionError(self, val, arg_type):
+        with pytest.raises(AssertionError):
+            _assert_types(val, arg_type=arg_type)
+
+
+    @pytest.mark.parametrize("val, arg_type, error_type", [
+    # parameters: val, arg_type, error_type
+        (1234, 'new-type', NotImplementedError), # arbitrary type not allowed
+        ])
+    def test__assert_types_other_errors(self, val, arg_type, error_type):
+        with pytest.raises(error_type):
+            _assert_types(val, arg_type=arg_type)
+
+
+    @pytest.mark.parametrize("val, arg_type, type_out", [
+    # parameters: val, arg_type, type_out
+
+        # Check some type casting
+        (1, 'float_tcast', float),
+        ('1.0', 'float_tcast', float),
+        (1.0, 'int_tcast', int),
+        ('1', 'int_tcast', int),
+        (True, 'int_tcast', int),
+        ('1',   'int_float_tcast', int),
+        ('1.0', 'int_float_tcast', float),
+        ('1',   'float_int_tcast', float),
+        ('1.0', 'float_int_tcast', float),
+        (1, 'str_tcast', str),
+        (1, 'bool_tcast', bool),
+        ((1,2,3), 'tuple-float-tcast', [float, float, float]),
+        ((1.0,2.0,3.0), 'tuple-int-tcast', [int, int, int]),
+        (('1','2','3.0'), 'tuple-int-float-tcast', [int, int, float]),
+        ])
+    def test__assert_types_casting_ouput_passing(self, val, arg_type, type_out):
+        """Make sure _assert_types returns empty (as opposed raising an 
+        error)."""
+        out = _assert_types(val, arg_type=arg_type)
+        if isinstance(out, (list, tuple)):
+            for element, element_out in zip(out, type_out):
+                assert element is element_out
+        else:
+            assert out is type_out
+
+
 
 
 @pytest.fixture
@@ -126,6 +185,26 @@ def simple_input_kwargs():
             'd' : None,
             'e' : float('nan'),
             }
+@pytest.fixture
+def tcasting_input_kwargs():
+    return {
+            'a': 1,
+            'b': '1.0',
+            'c': 1.0,
+            'd': '1',
+            'e': True,
+            'f': '1',
+            'g': '1.0',
+            'h': '1',
+            'i': '1.0',
+            'j': 1,
+            'k': 1,
+            'l': (1,2,3),
+            'm': (1.0,2.0,3.0),
+            'n': ('1','2','3.0'),
+            }
+
+
 class TestCheckKwarg:
     """Test check_kwarg."""
 
@@ -179,13 +258,53 @@ class TestCheckKwarg:
     @pytest.mark.parametrize(*simple_io_args)
     def test_pop(self, simple_input_kwargs, simple_key, simple_val):
         """Check that pop actually removes and returns kwarg."""
-        val = check_kwarg(simple_input_kwargs, simple_key, pop=True)
-        _assert_equal(val, simple_val)
+        _assert_equal(
+                check_kwarg(simple_input_kwargs, simple_key, pop=True),
+                simple_val)
         assert simple_key not in simple_input_kwargs
+
+
+    @pytest.mark.parametrize("key, val, arg_type, val_type", [
+    # parameters: key, val, arg_type, val_type
+
+        # Check some type casting
+        ('a', 1.0, 'float_tcast', float),
+        ('b', 1.0, 'float_tcast', float),
+        ('c', 1, 'int_tcast', int),
+        ('d', 1, 'int_tcast', int),
+        ('e', 1, 'int_tcast', int),
+        ('f', 1, 'int_float_tcast', int),
+        ('g', 1.0, 'int_float_tcast', float),
+        ('h', 1.0, 'float_int_tcast', float),
+        ('i', 1.0, 'float_int_tcast', float),
+        ('j', '1', 'str_tcast', str),
+        ('k', True, 'bool_tcast', bool),
+        ('l', (1.0,2.0,3.0), 'tuple-float-tcast', [float, float, float]),
+        ('m', (1,2,3), 'tuple-int-tcast', [int, int, int]),
+        ('n', (1,2,3.0), 'tuple-int-float-tcast', [int, int, float]),
+        ])
+    def test_type_casting(self, tcasting_input_kwargs, key, val, 
+            arg_type, val_type):
+        """Make sure check_kwarg returns the proper values and types for given 
+        type castings."""
+
+        out = check_kwarg(tcasting_input_kwargs, key, arg_type=arg_type)
+
+        if isinstance(val_type, (list, tuple)):
+            assert isinstance(out, (list, tuple))
+            assert isinstance(val, (list, tuple)) # Doesn't hurt....
+            for out_i, val_i, val_i_type in zip(out, val, val_type):
+                _assert_equal(out_i, val_i)
+                assert type(out_i) is type(val_i) is val_i_type
+        else:
+            _assert_equal(out, val)
+            assert type(out) is type(val) is val_type
+
 
 
 class TestCheckAddKwarg:
     """Test check_add_kwarg."""
+
     simple_io_args = ("simple_key, simple_val", [
         ('a', 0),
         ('b', 1),
@@ -194,12 +313,109 @@ class TestCheckAddKwarg:
         ('e', float('nan')),
         ])
 
-    #def test_
+    @pytest.mark.parametrize(*simple_io_args)
+    def test_simple(self, simple_input_kwargs, simple_key, simple_val):
+        """Check that keys in kwargs return the values."""
+        _assert_equal(
+                check_add_kwarg(simple_input_kwargs, simple_key),
+                simple_val
+                )
+
+    @pytest.mark.parametrize(*simple_io_args)
+    def test_default_provided_keys(self, simple_input_kwargs, simple_key, simple_val):
+        """Check that keys in the kwargs don't use the default value if not 
+        forced."""
+        _assert_equal(
+                check_add_kwarg(simple_input_kwargs, simple_key, 
+                    default='default'),
+                simple_val
+                )
+
+    @pytest.mark.parametrize(*simple_io_args)
+    def test_default_forced_keys(self, simple_input_kwargs, simple_key, simple_val):
+        """Check that keys in the kwargs use the default value when forced and 
+        updates the kwargs."""
+        _assert_equal(
+                check_add_kwarg(simple_input_kwargs, simple_key, 
+                    default='default', force=True),
+                'default'
+                )
+        _assert_equal(simple_input_kwargs[simple_key], 'default')
+
+    def test_missing_key(self, simple_input_kwargs):
+        """Check that a missing key uses the default value and that the 
+        missing key is added to the kwargs.
+        """
+        _assert_equal(
+                check_add_kwarg(simple_input_kwargs, 'missing_key'),
+                None
+                )
+        assert 'missing_key' in simple_input_kwargs
+
+    def test_default_missing_key(self, simple_input_kwargs):
+        """Check that a missing key uses the default value and that the 
+        missing key is added to the kwargs.
+        """
+        # Redundant test? Same as test_missing_key?
+        _assert_equal(
+                check_add_kwarg(simple_input_kwargs, 'missing_key', 
+                    default='default'),
+                'default'
+                )
+        assert 'missing_key' in simple_input_kwargs
+
+
+    @pytest.mark.parametrize("key, val, arg_type, val_type", [
+    # parameters: key, val, arg_type, val_type
+
+        # Check some type casting
+        ('a', 1.0, 'float_tcast', float),
+        ('b', 1.0, 'float_tcast', float),
+        ('c', 1, 'int_tcast', int),
+        ('d', 1, 'int_tcast', int),
+        ('e', 1, 'int_tcast', int),
+        ('f', 1, 'int_float_tcast', int),
+        ('g', 1.0, 'int_float_tcast', float),
+        ('h', 1.0, 'float_int_tcast', float),
+        ('i', 1.0, 'float_int_tcast', float),
+        ('j', '1', 'str_tcast', str),
+        ('k', True, 'bool_tcast', bool),
+        ('l', (1.0,2.0,3.0), 'tuple-float-tcast', [float, float, float]),
+        ('m', (1,2,3), 'tuple-int-tcast', [int, int, int]),
+        ('n', (1,2,3.0), 'tuple-int-float-tcast', [int, int, float]),
+        ])
+    def test_type_casting(self, tcasting_input_kwargs, key, val, 
+            arg_type, val_type):
+        """Make sure check_kwarg returns the proper values and types for given 
+        type castings."""
+
+        out = check_add_kwarg(tcasting_input_kwargs, key, arg_type=arg_type)
+
+        if isinstance(val_type, (list, tuple)):
+            assert isinstance(out, (list, tuple))
+            assert isinstance(val, (list, tuple)) # Doesn't hurt....
+            for out_i, val_i, val_i_type in zip(out, val, val_type):
+                _assert_equal(out_i, val_i)
+                assert type(out_i) is type(val_i) is val_i_type
+        else:
+            _assert_equal(out, val)
+            assert type(out) is type(val) is val_type
+
+
 
 class TestGetFus:
-    """Test get_check_kwarg_fu and get_check_add_kwarg_fu."""
+    """Test get_check_kwarg_fu and get_check_add_kwarg_fu.
+
+    How to test them?? These return a function handle, which I have no idea 
+    how to compare for truth.
+
+    These functions are so simple anyway....
+    """
     pass
 
 class TestKwargChecker:
-    """Test the KwargChecker class."""
+    """Test the KwargChecker class.
+    I don't really use the KwargChecker class anymore... Use at your own risk, 
+    or write tests for it."""
+
     pass
